858cea0b92b5704214a7ea6d318d83a221f6b25e
Liam Desmond <liamjdesmond84@gmail.com>
2023-04-24 18:00:25 -0500

    Messing with Data Types

--- Program.cs
+++ Program.cs
@@ -11,6 +11,12 @@ namespace DataTypes {
             // Declaring an array with pre-populated values
             // For Arrays initialized this way, the length is determined by the size of the given data
             int[] numArray2 = {1,2,3,4,6};
+
+            // Declaring an array without a set length, then initialize it after
+            int[] array3;
+            array3 = new int[] {1,3,5,7,9};
+
         }
-    }
+        
+
 }
