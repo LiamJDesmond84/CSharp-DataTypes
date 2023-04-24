b86541259cb7e03bb69c5753293c69783bb0c531
Liam Desmond <liamjdesmond84@gmail.com>
2023-04-23 21:32:54 -0500

    Messing with Data Types

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace DataTypes
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace DataTypes {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            // Declaring an array of length 5, initialized by default to all zeroes
+            int[] numArray = new int[5];
+            // Declaring an array with pre-populated values
+            // For Arrays initialized this way, the length is determined by the size of the given data
+            int[] numArray2 = {1,2,3,4,6};
         }
     }
 }
